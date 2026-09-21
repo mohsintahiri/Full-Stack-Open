7b7fa79ad2f797c0cfa92c4ae37ca13c5aedd5e7
mohsintahiri <mohsintahiirii@gmail.>
2026-09-21 20:24:06 +0200

    created content component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./Header"
+import Content from "./Content"
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -11,16 +12,8 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course}/>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Header course = {course}/>
+      <Content part1 = {part1} part2 = {part2} part3 = {part3} exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
