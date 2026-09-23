5867f5bf346e6855f609682f2550eb60c0883758
mohsintahiri <mohsintahiirii@gmail.>
2026-09-23 17:09:01 +0200

    Refactor Statistics component to handle no feedback case and simplify average and positive percentage calculations

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -4,14 +4,18 @@ const Statistics = ({good, neutral, bad})=>{
   const average = (good * 1 + bad * (-1)) / (good + neutral + bad)
   const positivePercentage = good * 100/ (good + neutral + bad)
 
+  if (good === 0 && bad === 0 && neutral === 0) {
+    return <p>No feedback given</p>
+  }
+
   return(
     <div>
       <Title text="statistics" />
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
-      <p>Average: {isNaN(average)? "There's no reviews, leave at least one to show the average" : average.toFixed(2)}</p>
-      <p>Positive: {isNaN(positivePercentage)? "There's no reviews, leave at least one to show the positive percentage" : positivePercentage.toFixed(2)}%</p>
+      <p>Average: {average.toFixed(2)}</p>
+      <p>Positive: {positivePercentage.toFixed(2)}%</p>
     </div>
   )
 }
