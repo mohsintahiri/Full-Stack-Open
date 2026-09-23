493e0236b7c8e75c595069de88d3a52b08604e9e
mohsintahiri <mohsintahiirii@gmail.>
2026-09-23 17:22:09 +0200

    Refactor Statistics and StatisticLine components to use table structure for displaying metrics

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,21 +1,25 @@
 import StatisticLine from "./StatisticLine"
 
 const Statistics = ({good, neutral, bad})=>{
-  const average = (good * 1 + bad * (-1)) / (good + neutral + bad)
-  const positivePercentage = good * 100/ (good + neutral + bad)
+  const total = good + neutral + bad 
+  const average = (good * 1 + bad * (-1)) / total
+  const positivePercentage = good * 100/ total
 
   if (good === 0 && bad === 0 && neutral === 0) {
     return <p>No feedback given</p>
   }
 
   return(
-    <div>
-      <StatisticLine text="good" value={good}/>
-      <StatisticLine text="neutral" value={neutral}/>
-      <StatisticLine text="bad" value={bad}/>
-      <StatisticLine text="average" value={average.toFixed(2)}/>
-      <StatisticLine text="positivePercentage" value={String(positivePercentage.toFixed(2)) + " %"}/>
-    </div>
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good}/>
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="all" value={total} />
+        <StatisticLine text="average" value={average.toFixed(2)}/>
+        <StatisticLine text="positive" value={String(positivePercentage.toFixed(2)) + " %"}/>
+      </tbody>
+    </table>
   )
 }
 
