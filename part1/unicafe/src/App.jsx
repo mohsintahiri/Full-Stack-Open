8f4dce2d4afed9ae138963400422efc9b25f9f22
mohsintahiri <mohsintahiirii@gmail.>
2026-09-23 16:54:09 +0200

    Add Button and Title components; implement state management in App component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,28 @@
+import { useState } from 'react'
+import Button from './Button'
+import Title from './Title'
+
 const App = () => {
-  
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
+
+  return (
+    <>
+      <Title text="unicafe" />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
+      <Title text="statistics" />
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </>
+  )
 }
 
 export default App
